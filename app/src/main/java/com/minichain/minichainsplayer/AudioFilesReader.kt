e03a2ca18404fc16b179520eb71d4f396fac1b89
Minichain <a.cadena@wearin.tech>
2024-01-21 12:46:42 +0100

    Being able to get audio files within music folder

--- app/src/main/java/com/minichain/minichainsplayer/AudioFilesReader.kt
+++ app/src/main/java/com/minichain/minichainsplayer/AudioFilesReader.kt
@@ -17,12 +17,14 @@ object AudioFilesReader {
       MediaStore.Audio.Media.DURATION,
       MediaStore.Audio.Media.SIZE
     )
+    val selection = MediaStore.Audio.Media.RELATIVE_PATH + " like ?"
+    val selectionArgs = arrayOf(folderPath)
     val sortOrder = "${MediaStore.Audio.Media.DISPLAY_NAME} ASC"
     context.contentResolver.query(
       uri,
       projection,
-      null,
-      null,
+      selection,
+      selectionArgs,
       sortOrder
     )?.use { cursor ->
       val idColumn = cursor.getColumnIndexOrThrow(MediaStore.Audio.Media._ID)
@@ -32,7 +34,7 @@ object AudioFilesReader {
       while (cursor.moveToNext()) {
         val fileId = cursor.getLong(idColumn)
         val fileName = cursor.getString(nameColumn)
-        val fileUri = ContentUris.withAppendedId(MediaStore.Audio.Media.EXTERNAL_CONTENT_URI, fileId)
+        val fileUri = ContentUris.withAppendedId(uri, fileId)
         val metadataRetriever = MediaMetadataRetriever().apply { setDataSource(context, fileUri) }
         audioFiles.add(
           SongData(
